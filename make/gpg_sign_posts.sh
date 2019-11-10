508eb0577bdc81b6b0d1e5d9f95a7002f78b0588
Sam Gleske <sam.mxracer@gmail.com>
2019-11-10 13:58:56 -0500

    Update GPG signatures

--- make/gpg_sign_posts.sh
+++ make/gpg_sign_posts.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-branch="$(bundle exec ruby ./make/get_yaml_key.rb github_branch)"
+branch="$(docker run --rm ruby-blog bundle exec ruby ./make/get_yaml_key.rb github_branch)"
 merge_base="$(git merge-base HEAD origin/"${branch}")"
 git diff --name-status "${merge_base}" HEAD |
   awk '$0 ~ /_posts\/.*\.md$/ {print $2}' |
